f7c0547da201dc20ff5f9e3bfd82d535647b3275
Michael <michaelolivares123@gmail.com>
2023-01-05 09:34:52 -0500

    feat(): add before each

--- test/unit/todo.controller.test.js
+++ test/unit/todo.controller.test.js
@@ -4,16 +4,19 @@ const httpMocks = require("node-mocks-http");
 const newTodo = require("../mock-data/new-todo.json");
 
 TodoModel.create = jest.fn();
+let req, res, next;
+beforeEach(() => {
+  //reset in every test
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
 
 describe("TodoController.createTodo", () => {
   it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
   it("should call TodoModel.create", () => {
-    let req, res, next;
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = null;
     req.body = newTodo;
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(newTodo);
