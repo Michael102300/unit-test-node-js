19c700af3624837805f1579d75a525efd537a63c
Michael <michaelolivares123@gmail.com>
2023-01-05 20:15:14 -0500

    feat(): add integration test in getTodos

--- test/integration/todo.controller.int.test.js
+++ test/integration/todo.controller.int.test.js
@@ -5,6 +5,14 @@ const newTodo = require("../mock-data/new-todo.json");
 const endpointUrl = "/todos/";
 
 describe(endpointUrl, () => {
+  test("GET" + endpointUrl, async () => {
+    const res = await request(app).get(endpointUrl);
+
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBeTruthy();
+    expect(res.body[0].title).toBeDefined();
+    expect(res.body[0].done).toBeDefined();
+  });
   it("POST" + endpointUrl, async () => {
     const res = await request(app).post(endpointUrl).send(newTodo);
     expect(res.statusCode).toBe(201);
