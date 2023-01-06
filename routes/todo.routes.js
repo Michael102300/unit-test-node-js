afa0915f86c11dc75c2dcef6a786653cc543b773
Michael <michaelolivares123@gmail.com>
2023-01-06 15:27:00 -0500

    feat(): add test integration update route

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,11 +3,13 @@ const {
   createTodo,
   getTodos,
   getTodoById,
+  updateTodo,
 } = require("../controllers/todo.controller");
 const router = express.Router();
 
 router.post("/", createTodo);
 router.get("/", getTodos);
 router.get("/:todoId", getTodoById);
+router.put("/:todoId", updateTodo);
 
 module.exports = router;
--- test/integration/todo.controller.int.test.js
+++ test/integration/todo.controller.int.test.js
@@ -1,11 +1,10 @@
 const request = require("supertest");
-const { response } = require("../../app");
 const app = require("../../app");
 const newTodo = require("../mock-data/new-todo.json");
 
 const endpointUrl = "/todos/";
 
-let firstTodo;
+let firstTodo, newTodoId;
 
 describe(endpointUrl, () => {
   test("GET" + endpointUrl, async () => {
@@ -37,6 +36,7 @@ describe(endpointUrl, () => {
     expect(res.statusCode).toBe(201);
     expect(res.body.title).toBe(newTodo.title);
     expect(res.body.done).toBe(newTodo.done);
+    newTodoId = res.body._id;
   });
   it("should return error 500 on malformed data with POST", async () => {
     const res = await request(app)
@@ -46,5 +46,14 @@ describe(endpointUrl, () => {
     expect(res.body).toStrictEqual({
       message: "Todo validation failed: done: Path `done` is required.",
     });
+  });
+  it("PUT" + endpointUrl, async () => {
+    const testData = { title: "Make integration test to PUT", done: true };
+    const res = await request(app)
+      .put(endpointUrl + newTodoId)
+      .send(testData);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.title).toBe(testData.title);
+    expect(res.body.done).toBe(testData.done);
   });
 });
